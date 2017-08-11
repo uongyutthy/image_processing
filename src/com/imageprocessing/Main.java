2b3cb156a4821a12618846040fa21c32e9ff3f6c
Uong Yutthy <uyutthy.pbh@gmail.com>
2017-08-11 14:21:37 +0700

    first commit

---
+++ src/com/imageprocessing/Main.java
@@ -0,0 +1,23 @@
+package com.imageprocessing;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("view/image-processing.fxml"));
+        primaryStage.setTitle("Image Processing");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/com/imageprocessing/controller/ImageProcessingController.java
@@ -0,0 +1,11 @@
+package com.imageprocessing.controller;
+
+import javafx.event.ActionEvent;
+
+public class ImageProcessingController {
+    public void alert(ActionEvent actionEvent) {
+        System.out.println("herlo");
+    }
+
+
+}
